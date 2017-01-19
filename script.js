72514d8627846e0b074f7e7f67c5175793e83bea
raffaelbr08 <rafael.hp8@gmail.com>
2017-01-19 21:00:30 -0200

    dsa

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var array = [2,3,4,12,6,7],
-	resultado1;
 
-	function par (elemento){
-		return (elemento / 2==0);
-	}
 
-	console.log(array);
-	resultado1 = array.every (par);
-	console.log(resultado1);
+// if else
+
+var notaEnem = 6;
+var resultado = "";
+
+if(notaEnem >=6){
+	resultado = "Aprovado"
+}else{
+	if(notaEnem>=5)
+	resultado = "Recuperaçao"
+	else
+		resultado = "Reprovado"
+}
+
+console.log(resultado);
